7a37b0c07420e80f1caad37438bc7d4117b0343e
nishant <nishantkudale04@gmail.com>
2026-02-09 23:08:15 +0530

    created vite project

---
+++ foodies/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menubar from './components/Menubar/Menubar';
+
+const App = () => {
+  return (
+    <div>
+      <Menubar></Menubar>
+    </div>
+  )
+}
+
+export default App;
