8f7f4908420e6fc62f7a5fa943d877e5fa65acb6
nishant <nishantkudale04@gmail.com>
2026-02-09 23:09:19 +0530

    created Menubar

---
+++ foodies/src/components/Menubar/Menubar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Menubar.css";
+import { assets } from "../../assets/assets";
+
+const Menubar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <img src={assets.logo} alt="" className="mx-4" height={48} width={48} />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link " href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Explore
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Constact us
+              </a>
+            </li>
+            
+          </ul>
+          <div className="d-flex align-items-center                  ">
+            <div className="position-relative">
+                <img src={assets.cart} alt="" height={32} width={32}  className="position-relative"/>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
+                  2
+                </span>
+             </div>
+             <button className="btn btn-outline-primary ms-3" type="submit">
+               Login
+             </button>
+             <button className="btn btn-outline-success ms-3" type="submit">
+               Register
+             </button>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Menubar;
