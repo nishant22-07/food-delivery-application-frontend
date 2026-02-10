d1b4eb9a321be78a64df3600a0fa15f2ee42793a
nishant <nishantkudale04@gmail.com>
2026-02-10 11:21:13 +0530

    wrap App component with BrowserRouter for routing support

--- foodies/src/main.jsx
+++ foodies/src/main.jsx
@@ -6,9 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter}  from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+
   
 )
