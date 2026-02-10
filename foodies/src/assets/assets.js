579355f1f8af3dec0e7331d27455bcd3a1d91161
nishant <nishantkudale04@gmail.com>
2026-02-10 15:41:41 +0530

    add new food category images and export categories array

--- foodies/src/assets/assets.js
+++ foodies/src/assets/assets.js
@@ -1,7 +1,33 @@
 import logo from './logo.png';
 import cart from './cart.png';
+import biryani from './biryani.png';
+import burger from './burger.png';
+import cakes from './cakes.png'
+import pizza from './pizza.png';
+import rolls from './rolls.png';
+import salad from './salad.png';
+import icecream from './icecream.png';
+
 
 export const assets = {
-  logo,
-  cart
+    logo,
+    cart,
+    biryani,
+    burger,
+    cakes,
+    pizza,
+    rolls,
+    salad,
+    icecream
+
 };
+
+export const categories = [
+    { category: 'Biryani', icon: biryani },
+    { category: 'Burger', icon: burger },
+    { category: 'Cakes', icon: cakes },
+    { category: 'Pizza', icon: pizza },
+    { category: 'Rolls', icon: rolls },
+    { category: 'Salad', icon: salad },
+    { category: 'Ice Cream', icon: icecream }
+]
