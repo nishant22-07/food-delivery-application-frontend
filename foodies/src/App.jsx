f8d3c94b6a6004f50f7367c524e63f89fdeae7e6
nishant <nishantkudale04@gmail.com>
2026-02-10 11:21:23 +0530

    add routing with React Router for Home, ExploreFood, and Contact pages

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Menubar from './components/Menubar/Menubar';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import ExploreFood from "./pages/ExploreFood/ExploreFood.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+
+
 
 const App = () => {
   return (
     <div>
       <Menubar></Menubar>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+            <Route path="/explore" element={<ExploreFood/>} />
+            <Route path="/contact" element={<Contact/>} />
+
+        </Routes>
     </div>
   )
 }
