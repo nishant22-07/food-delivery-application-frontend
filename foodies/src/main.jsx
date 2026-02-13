5f11f1165364e364f81bec2a5e9b2e28af30a3d4
nishant <nishantkudale04@gmail.com>
2026-02-13 09:41:03 +0530

    wrap App component with StoreContextProvider for state management

--- foodies/src/main.jsx
+++ foodies/src/main.jsx
@@ -7,10 +7,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter}  from "react-router-dom";
+import {StoreContextProvider} from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      <App />
+      <StoreContextProvider>
+           <StrictMode>
+              <App />
+          </StrictMode>
+      </StoreContextProvider>
   </BrowserRouter>
 
   
