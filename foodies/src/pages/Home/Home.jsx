c51b66e3ff93219048374892520f5ee2ee16a1a4
nishant <nishantkudale04@gmail.com>
2026-02-13 09:40:52 +0530

    add FoodDisplay component to Home

--- foodies/src/pages/Home/Home.jsx
+++ foodies/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../../components/Hader/Header.jsx";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx";
 
 const Home = () => {
     return (
         <main className="container">
             <Header></Header>
+            <FoodDisplay/>
             <ExploreMenu></ExploreMenu>
         </main>
     )
