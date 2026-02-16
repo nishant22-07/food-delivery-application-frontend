7207eeb0afe408ddda7bb9ff881e8a3dc31ec8ae
nishant <nishantkudale04@gmail.com>
2026-02-16 12:48:21 +0530

    fetch food items from context and display them in FoodDisplay component

--- foodies/src/components/FoodDisplay/FoodDisplay.jsx
+++ foodies/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {StoreContext} from "../../context/StoreContext.jsx";
+import FoodItem from "../FoodItem/FoodItem.jsx";
+
 
 const FoodDisplay = () => {
+    const {foodList} = useContext(StoreContext);
     return (
-        <div>FoodDisplay</div>
-    )
+        <div className="container">
+            <div className="row">
+                {foodList.length > 0 ? (foodList.map((food, index) => (
+                    <FoodItem key={index}
+                              name={food.name}
+                              description={food.description}
+                              id={food.id}
+                              price={food.price}
+                              imageUrl={food.imageUrl}
+                    />
+
+                ))) : (
+                    <div className="col-12 d-flex justify-content-center align-items-center">
+                        <p className="fs-4">No food items available</p>
+                    </div>)}
+
+            </div>
+        </div>)
 }
 export default FoodDisplay;
