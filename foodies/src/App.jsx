4b181b3631f1f1b4540a563e9fa02427dbc5a4b5
nishant <nishantkudale04@gmail.com>
2026-02-18 22:01:37 +0530

    update routing in App component to use ExploreFood and add Cart page

--- foodies/src/App.jsx
+++ foodies/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Menubar from './components/Menubar/Menubar';
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
-import Explore from "./pages/ExploreFood/Explore.jsx";
+import ExploreFood from "./pages/ExploreFood/ExploreFood.jsx";
 import Contact from "./pages/Contact/Contact.jsx";
 import Header from "./components/Hader/Header.jsx";
 import FoodDetails from "./pages/FoodDetails/FoodDetails.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
 
 
 
@@ -16,9 +17,10 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Home/>} />
-            <Route path="/explore" element={<Explore/>} />
+            <Route path="/explore" element={<ExploreFood/>} />
             <Route path="/contact" element={<Contact/>} />
             <Route path="/food/:id" element={<FoodDetails/>} />
+            <Route path="/cart" element={<Cart/>} />
 
         </Routes>
     </div>
