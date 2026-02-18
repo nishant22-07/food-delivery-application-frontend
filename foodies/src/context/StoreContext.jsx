22d8c64bb62b636344dec697fead206d3d99bd3c
nishant <nishantkudale04@gmail.com>
2026-02-19 00:26:57 +0530

    feat: add quantity management functions to StoreContext

--- foodies/src/context/StoreContext.jsx
+++ foodies/src/context/StoreContext.jsx
@@ -6,11 +6,28 @@ export const StoreContext = createContext(null);
 export const StoreContextProvider = (props) => {
 
     const[foodList, setFoodList] = useState([]);
+    const[quantities, setQuantities] = useState({}); // This will hold the quantities of each food item
 
+    const increaseQty = (foodId) => {
+        setQuantities((prev) => ({...prev, [foodId]: (prev[foodId] || 0) + 1}));
+    }
+    const decreaseQty = (foodId) => {
+        setQuantities((prev) => ({...prev, [foodId]: prev[foodId] > 0 ? prev[foodId] - 1 : 0 }));
+    }
 
-
+    const removeFromCart = (foodId) => {
+        setQuantities((prevQuantities) => {
+            const updatedQuantities = {...prevQuantities};
+            delete updatedQuantities[foodId]; // This will remove the food item from the quantities object
+            return updatedQuantities;
+        });
+    }
     const contextValue = {
-        foodList
+        foodList,
+        increaseQty,
+        decreaseQty,
+        quantities,
+        removeFromCart
     }; // You can add any state or functions you want to share across components here
 
     useEffect(() => {
