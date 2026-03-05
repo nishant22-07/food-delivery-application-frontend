671c0954d939f6f040a67fe8f604c3fd780e6b37
nishant <nishantkudale04@gmail.com>
2026-03-05 19:37:00 +0530

    feat: add filtering functionality to FoodDisplay component based on category and search text

--- foodies/src/components/FoodDisplay/FoodDisplay.jsx
+++ foodies/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,13 +2,19 @@ import React, {useContext} from 'react';
 import {StoreContext} from "../../context/StoreContext.jsx";
 import FoodItem from "../FoodItem/FoodItem.jsx";
 
+const FoodDisplay = ({category , searchText}) => {
 
-const FoodDisplay = () => {
     const {foodList} = useContext(StoreContext);
+    const filteredFoods = foodList.filter(food => (
+        (category === 'All' || food.category === category) &&
+        food.name.toLowerCase().includes(searchText.toLowerCase())
+
+    ));
+
     return (
         <div className="container">
             <div className="row">
-                {foodList.length > 0 ? (foodList.map((food, index) => (
+                {filteredFoods.length > 0 ? (filteredFoods.map((food, index) => (
                     <FoodItem key={index}
                               name={food.name}
                               description={food.description}
