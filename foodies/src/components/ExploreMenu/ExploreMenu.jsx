c1a1cb78a30831f6da6c71329fd1bab7181c5b16
nishant <nishantkudale04@gmail.com>
2026-03-05 19:35:36 +0530

    feat: update ExploreMenu component to improve category selection and styling

--- foodies/src/components/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-
 import {categories} from "../../assets/assets.js";
 import './ExploreMenu.css'
-import {useRef} from "react";
+import React, {useRef} from "react";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
     const menuRef = useRef(null);
-
-     const scrollLeft =  ()=>{
-         if(menuRef.current){
-                menuRef.current.scrollBy({left:-200,behavior:"smooth"})
-         }
-     }
-    const scrollRight =  ()=>{
-        if(menuRef.current){
-            menuRef.current.scrollBy({left:200,behavior:"smooth"})
+    const scrollLeft = () => {
+        if (menuRef.current) {
+            menuRef.current.scrollBy({left: -200, behavior: "smooth"})
+        }
+    }
+    const scrollRight = () => {
+        if (menuRef.current) {
+            menuRef.current.scrollBy({left: 200, behavior: "smooth"})
         }
     }
 
@@ -26,18 +23,16 @@ const ExploreMenu = () => {
                 <div className="d-flex">
                     <i className="bi bi-arrow-left-circle scroll-icon" onClick={scrollLeft}></i>
                     <i className="bi bi-arrow-right-circle scroll-icon" onClick={scrollRight}></i>
-
-
                 </div>
 
             </h1>
             <p>Explore curated lists of dishes from top categories</p>
             <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list" ref={menuRef}>
                 {
-                    categories.map((item,index) => {
+                    categories.map((item, index) => {
                         return (
-                            <div key={index} className="text-center explore-menu-list-item" >
-                                <img src={item.icon} alt="" className="rounded-circle" height={150} width={128}/>
+                            <div key={index} className="text-center explore-menu-list-item" onClick={() => setCategory(prev=>prev === item.category?'All':item.category)}>
+                                <img src={item.icon} alt="" className={item.category === category ? 'rounded-circle active' : 'rounded-circle'} height={128} width={128}/>
                                 <p className="mt-2 fw-bold">{item.category}</p>
                             </div>
                         )
