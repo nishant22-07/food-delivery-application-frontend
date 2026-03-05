a1eef6f7c83d6c6bf14b59117b53d5a2a055569a
nishant <nishantkudale04@gmail.com>
2026-03-05 19:44:26 +0530

    feat: add category state to Home component and pass it to ExploreMenu and FoodDisplay

--- foodies/src/pages/Home/Home.jsx
+++ foodies/src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../../components/Hader/Header.jsx";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx";
 
 const Home = () => {
+    const [category, setCategory] = useState('All');
+
     return (
         <main className="container">
             <Header></Header>
 
-            <ExploreMenu></ExploreMenu>
-            <FoodDisplay/>
+            <ExploreMenu category={category} setCategory={setCategory}/>
+            <FoodDisplay category={category} searchText={''}/>
         </main>
     )
 }
