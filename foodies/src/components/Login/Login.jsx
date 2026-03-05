587b20d6b7409e5d570c450e4d09c87288e06018
nishant <nishantkudale04@gmail.com>
2026-03-05 19:45:50 +0530

    feat: implement login functionality with form handling and error notifications

--- foodies/src/components/Login/Login.jsx
+++ foodies/src/components/Login/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import "./Login.css"
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {login} from "../../Service/authService.js";
+import {StoreContext} from "../../context/StoreContext.jsx";
+import {toast} from "react-toastify";
 
 const Login = () => {
+    const {setToken,loadCartData} = useContext(StoreContext);
+    const navigate = useNavigate()
+    const [data,setData] = useState({
+        email:'',
+        password:''
+    });
+    
+    const onChangeHandler = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setData(data =>({...data, [name]:value}));
+    }
+
+    const onSubmitHandler = async (e)=>{
+        e.preventDefault();
+        try{
+            const response = await login(data);
+            if(response.status === 200){
+                setToken(response.data.token);
+                localStorage.setItem("token",response.data.token);
+                await loadCartData(response.data.token)
+                navigate('/');
+            }else{
+                toast.error('Unable to login. Please try again')
+            }
+        }catch(error){
+            console.log("Unable to login",error)
+            toast.error('Unable to login. Please try again')
+
+        }
+    }
     return (
         <div className="container login-container">
             <div className="row">
@@ -10,15 +44,29 @@ const Login = () => {
                     <div className="card border-0 shadow rounded-3 my-5">
                         <div className="card-body p-4 p-sm-5">
                             <h5 className="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
-                            <form>
+                            <form onSubmit={onSubmitHandler}>
                                 <div className="form-floating mb-3">
-                                    <input type="email" className="form-control" id="floatingInput"
-                                           placeholder="name@example.com"/>
+                                    <input type="email"
+                                           className="form-control"
+                                           id="floatingInput"
+                                           placeholder="name@example.com"
+                                           name="email"
+                                           onChange={onChangeHandler}
+                                           value={data.email}
+                                           required
+                                    />
                                     <label htmlFor="floatingInput">Email address</label>
                                 </div>
                                 <div className="form-floating mb-3">
-                                    <input type="password" className="form-control" id="floatingPassword"
-                                           placeholder="Password"/>
+                                    <input type="password"
+                                           className="form-control"
+                                           id="floatingPassword"
+                                           placeholder="Password"
+                                           name="password"
+                                           onChange={onChangeHandler}
+                                           value={data.password}
+                                           required
+                                    />
                                     <label htmlFor="floatingPassword">Password</label>
                                 </div>
 
