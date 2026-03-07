0beaaf4a18c95ef164035d2247f628b3b6de5ef6
nishant <nishantkudale04@gmail.com>
2026-03-08 01:22:53 +0530

    feat: enhance login component with background image and reset functionality

--- foodies/src/components/Login/Login.jsx
+++ foodies/src/components/Login/Login.jsx
@@ -4,95 +4,135 @@ import {Link, useNavigate} from "react-router-dom";
 import {login} from "../../Service/authService.js";
 import {StoreContext} from "../../context/StoreContext.jsx";
 import {toast} from "react-toastify";
+import {assets} from "../../assets/assets.js";
 
 const Login = () => {
-    const {setToken,loadCartData} = useContext(StoreContext);
+
+    const {setToken, loadCartData} = useContext(StoreContext);
     const navigate = useNavigate()
+
     const [data,setData] = useState({
         email:'',
         password:''
     });
-    
-    const onChangeHandler = (e) =>{
+
+    const onChangeHandler = (e)=>{
         const name = e.target.name;
         const value = e.target.value;
-        setData(data =>({...data, [name]:value}));
+        setData(data => ({...data, [name]:value}));
     }
 
     const onSubmitHandler = async (e)=>{
         e.preventDefault();
+
         try{
             const response = await login(data);
+
             if(response.status === 200){
                 setToken(response.data.token);
                 localStorage.setItem("token",response.data.token);
                 await loadCartData(response.data.token)
                 navigate('/');
             }else{
-                toast.error('Unable to login. Please try again')
+                toast.error("Unable to login. Please try again");
             }
+
         }catch(error){
             console.log("Unable to login",error)
-            toast.error('Unable to login. Please try again')
-
+            toast.error("Unable to login. Please try again");
         }
     }
+
+    // RESET FUNCTION
+    const resetHandler = ()=>{
+        setData({
+            email:"",
+            password:""
+        })
+    }
+
     return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                    <div className="card border-0 shadow rounded-3 my-5">
-                        <div className="card-body p-4 p-sm-5">
-                            <h5 className="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
-                            <form onSubmit={onSubmitHandler}>
-                                <div className="form-floating mb-3">
-                                    <input type="email"
-                                           className="form-control"
-                                           id="floatingInput"
-                                           placeholder="name@example.com"
-                                           name="email"
-                                           onChange={onChangeHandler}
-                                           value={data.email}
-                                           required
-                                    />
-                                    <label htmlFor="floatingInput">Email address</label>
-                                </div>
-                                <div className="form-floating mb-3">
-                                    <input type="password"
-                                           className="form-control"
-                                           id="floatingPassword"
-                                           placeholder="Password"
-                                           name="password"
-                                           onChange={onChangeHandler}
-                                           value={data.password}
-                                           required
-                                    />
-                                    <label htmlFor="floatingPassword">Password</label>
-                                </div>
 
-                                <div className="d-grid">
-                                    <button className="btn btn-outline-primary btn-login text-uppercase "
-                                            type="submit">Sign
-                                        in
-                                    </button>
-                                </div>
-                                <div className="d-grid mt-2">
-                                    <button className="btn btn-outline-danger btn-login text-uppercase "
-                                            type="submit">Reset
-                                    </button>
-                                </div>
-                                <div>
-                                    Don't have an account? <Link to={"/register"}>Sign Up</Link>
-                                </div>
+        <div
+            className="login-page"
+            style={{
+                backgroundImage: `url(${assets.background})`
+            }}
+        >
 
+            <div className="container login-container">
+                <div className="row">
+                    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
 
-                            </form>
+                        <div className="card border-0 shadow rounded-3 my-5">
+                            <div className="card-body p-4 p-sm-5">
+
+                                <h5 className="card-title text-center mb-5 fw-light fs-5">
+                                    Sign In
+                                </h5>
+
+                                <form onSubmit={onSubmitHandler}>
+
+                                    <div className="form-floating mb-3">
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="floatingInput"
+                                            placeholder="name@example.com"
+                                            name="email"
+                                            value={data.email}
+                                            onChange={onChangeHandler}
+                                            required
+                                        />
+                                        <label htmlFor="floatingInput">Email address</label>
+                                    </div>
+
+                                    <div className="form-floating mb-3">
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="floatingPassword"
+                                            placeholder="Password"
+                                            name="password"
+                                            value={data.password}
+                                            onChange={onChangeHandler}
+                                            required
+                                        />
+                                        <label htmlFor="floatingPassword">Password</label>
+                                    </div>
+
+                                    <div className="d-grid">
+                                        <button
+                                            className="btn btn-outline-primary btn-login text-uppercase"
+                                            type="submit">
+                                            Sign In
+                                        </button>
+                                    </div>
+
+                                    <div className="d-grid mt-2">
+                                        <button
+                                            type="button"
+                                            onClick={resetHandler}
+                                            className="btn btn-outline-danger btn-login text-uppercase">
+                                            Reset
+                                        </button>
+                                    </div>
+
+                                    <div className="mt-3 text-center">
+                                        Don't have an account? <Link to="/register">Sign Up</Link>
+                                    </div>
+
+                                </form>
+
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
-        </div>
 
+        </div>
     )
 }
+
 export default Login
